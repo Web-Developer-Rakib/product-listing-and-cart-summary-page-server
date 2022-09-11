904a9a227aeef0cb42d9df960fad20455a2c26d2
Web-Developer-Rakib <rakibodesk7@gmail.com>
2022-09-11 07:39:43 +0600

    Search API created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -27,6 +28,28 @@ const run = async () => {
       .db("product_listing")
       .collection("products");
     const cartCollection = client.db("product_listing").collection("cart");
+    //Get all Products
+    app.get("/products", async (req, res) => {
+      const searchtxt = req.query.name;
+      const category = req.query.category;
+      const size = req.query.size;
+      console.log(searchtxt);
+      let result;
+      if (searchtxt === "" || category === "" || size === "") {
+        const query = {};
+        const cursor = productsCollection.find(query);
+        result = await cursor.toArray();
+      } else {
+        const query = [
+          { name: new RegExp(searchtxt, "i") },
+          { category: category },
+          { size: size },
+        ];
+        const cursor = productsCollection.find(query);
+        result = await cursor.toArray();
+      }
+      res.send(result);
+    });
   } finally {
     // Server is running
   }
